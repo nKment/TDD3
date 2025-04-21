2912966369622cbbab02e44c67373e82fa8d0653
Nicolas Kment <nicolas.kment@gmail.com>
2025-04-21 12:35:46 +0200

    Test (green): return empty trip list if logged user is not a friend (by Nicolas Kment)

--- test/TripServiceSpec.js
+++ test/TripServiceSpec.js
@@ -3,6 +3,7 @@ import sinon from 'sinon';
 import TripService from '../src/TripService.js';
 import User from '../src/User.js';
 import UserSession from '../src/UserSession.js';
+import TripDAO from '../src/TripDAO.js';
 
 const { expect } = chai;
 
@@ -23,5 +24,18 @@ describe('TripService', () => {
         const someUser = new User();
 
         expect(() => tripService.getTripsByUser(someUser)).to.throw('User not logged in.');
+    });
+
+    it('should return empty trip list if logged user is not a friend', () => {
+        const loggedUser = new User();
+        const requestedUser = new User(); // keine Freunde → loggedUser ist KEIN Freund
+
+        sinon.stub(UserSession, 'getLoggedUser').returns(loggedUser);
+        const tripDAOSpy = sinon.stub(TripDAO, 'findTripsByUser');
+
+        const result = tripService.getTripsByUser(requestedUser);
+
+        expect(result).to.be.an('array').that.is.empty;
+        expect(tripDAOSpy.notCalled).to.be.true; // DAO darf NICHT aufgerufen werden!
     });
 });
