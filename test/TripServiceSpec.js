0a0a7dd8fb2eb92cca60884d61ce76d763c85e43
Nicolas Kment <nicolas.kment@gmail.com>
2025-04-21 12:15:32 +0200

    Test (green): throw error if no user is logged in (by Nicolas Kment)

--- test/TripServiceSpec.js
+++ test/TripServiceSpec.js
@@ -1,13 +1,27 @@
-"use strict"
+import chai from 'chai';
+import sinon from 'sinon';
+import TripService from '../src/TripService.js';
+import User from '../src/User.js';
+import UserSession from '../src/UserSession.js';
 
-import chai from 'chai'
-import TripService from '../src/TripService.js'
-const {expect} = chai
+const { expect } = chai;
 
 describe('TripService', () => {
+    let tripService;
 
-    it('should... ', () => {
-        expect(2+2).to.equal(5)
-    })
+    beforeEach(() => {
+        tripService = new TripService();
+    });
 
-})
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    it('should throw an error if no user is logged in', () => {
+        sinon.stub(UserSession, 'getLoggedUser').returns(null);
+
+        const someUser = new User();
+
+        expect(() => tripService.getTripsByUser(someUser)).to.throw('User not logged in.');
+    });
+});
